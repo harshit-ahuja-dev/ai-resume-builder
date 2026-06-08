c56d3f9b3e074f5feb59874def33ce0e56882058
Harshit Ahuja <harshitahuja.it@gmail.com>
2026-06-08 23:26:15 +0530

    Initial project setup with routing and Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import { LayoutDashboard } from 'lucide-react'
+import Dashboard from './pages/dashboard'
+import Preview from './pages/Preview'
+import ResumeBuilder from './pages/ResumeBuilder'
+import Layout from './pages/Layout'
+import Login from './pages/Login'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+
+        <Route path='app' element={<Layout />}>
+          <Route index element={<Dashboard />}/>
+          <Route path='builder/:resumeId' element={<ResumeBuilder />}/>
+        </Route>
+
+        <Route path='view/:resumeId' element={<Preview />} />
+        <Route path='login' element={<Login />} />
+
+      </Routes>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>Layout Page</h1>
+      <div>
+        <Outlet>
+            
+        </Outlet>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
